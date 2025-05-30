126861c10079453aea210e1079514548f59fa250
clarissacl <161193775+clarissacl@users.noreply.github.com>
2025-05-30 22:51:18 +0700

    Update PremiumCustomer.java
    Extract buyGame

--- src/PremiumCustomer.java
+++ src/PremiumCustomer.java
@@ -1,62 +1,71 @@
-
 import java.util.Map;
 import java.util.Scanner;
 
 public class PremiumCustomer extends Customer {
-    private double discountRate;
+    private DiscountRate discountRate;
 
-    public PremiumCustomer(String username, String password, double discountRate) {
+    public PremiumCustomer(Username username, Password password, DiscountRate discountRate) {
         super(username, password);
-        super.setRole("premium_customer");
+        super.setRole(UserRole.PREMIUM_CUSTOMER);
         this.discountRate = discountRate;
-        if (discountRate < 0 || discountRate >= 1) {
-            System.out.println("Warning: Invalid discount rate " + discountRate + ". Setting to 0.1 (10%).");
-            this.discountRate = 0.1;
-        }
     }
 
-    public double getDiscountRate() {
+    public DiscountRate getDiscountRate() {
         return discountRate;
     }
 
-    public void setDiscountRate(double discountRate) {
-        if (discountRate < 0 || discountRate >= 1) {
-            System.out.println("Warning: Invalid discount rate " + discountRate + ". Not changed.");
-            return;
-        }
+    public void setDiscountRate(DiscountRate discountRate) {
         this.discountRate = discountRate;
     }
 
     @Override
-    public void buyGame(String storeName, String gameName, StoreService storeService) {
+    public void buyGame(StoreName storeName, GameName gameName, StoreService storeService) {
         Game gameToBuy = storeService.getGameFromStore(storeName, gameName);
 
-        if (gameToBuy != null) {
-            double originalPrice = gameToBuy.getPrice();
-            double discountedPrice = originalPrice * (1 - this.discountRate);
-
-            System.out.println("Premium Member Price for " + gameName + ": $" + String.format("%.2f", discountedPrice) +
-                               " (Original: $" + String.format("%.2f", originalPrice) + ")");
-
-            if (super.getBalance() >= discountedPrice) {
-                if (adjustBalance(-discountedPrice)) {
-                    super.getOwnedGames().add(gameName);
-                    TransactionLogger.logTransaction(getUsername(), "PREMIUM_PURCHASE", discountedPrice, gameName + " from " + storeName);
-                    System.out.println("Premium game '" + gameName + "' purchased successfully from '" + storeName + "'!");
-                    System.out.println("It has been added to your library. New balance: $" + String.format("%.2f", getBalance()));
-                } else {
-                    System.out.println("Purchase failed during balance adjustment.");
-                }
-            } else {
-                System.out.println("Insufficient balance for premium purchase!");
-            }
-        } else {
+        if (gameToBuy == null) {
             System.out.println("Store or game not found!");
+            return;
+        }
+
+        Price originalPrice = gameToBuy.getPrice();
+        Price discountedPrice = calculateDiscountedPrice(originalPrice);
+
+        displayPriceInformation(gameName, originalPrice, discountedPrice);
+
+        if (canAffordGame(discountedPrice)) {
+            processPurchase(gameName, storeName, discountedPrice);
+        } else {
+            System.out.println("Insufficient balance for premium purchase!");
+        }
+    }
+
+    private Price calculateDiscountedPrice(Price originalPrice) {
+        double discountedPriceValue = originalPrice.getValue() * (1 - this.discountRate.getValue());
+        return new Price(discountedPriceValue);
+    }
+
+    private void displayPriceInformation(GameName gameName, Price originalPrice, Price discountedPrice) {
+        System.out.println("Premium Member Price for " + gameName.getValue() + ": $" + String.format("%.2f", discountedPrice.getValue()) +
+                           " (Original: $" + String.format("%.2f", originalPrice.getValue()) + ")");
+    }
+
+    private boolean canAffordGame(Price price) {
+        return super.getBalance() >= price.getValue();
+    }
+
+    private void processPurchase(GameName gameName, StoreName storeName, Price price) {
+        if (adjustBalance(-price.getValue())) {
+            super.getOwnedGames().add(gameName);
+            TransactionLogger.logTransaction(getUsername(), "PREMIUM_PURCHASE", price, gameName.getValue() + " from " + storeName.getValue());
+            System.out.println("Premium game '" + gameName.getValue() + "' purchased successfully from '" + storeName.getValue() + "'!");
+            System.out.println("It has been added to your library. New balance: $" + String.format("%.2f", getBalance()));
+        } else {
+            System.out.println("Purchase failed during balance adjustment.");
         }
     }
 
     @Override
     public void performAdminAction(Scanner scanner, UserManager userManager, StoreService storeService) {
-        System.out.println("Premium Customers (" + getUsername() + ") cannot perform admin actions.");
+        System.out.println("Premium Customers (" + getUsername().getValue() + ") cannot perform admin actions.");
     }
 }
